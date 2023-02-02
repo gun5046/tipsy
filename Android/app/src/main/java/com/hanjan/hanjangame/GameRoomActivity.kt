d5407cca76755c7cb5a9640e8cdf3924b0668682
송관재 <kosdjs39@gmail.com>
2023-02-02 13:22:25 +0900

    Feat: [S08P12D207-225] 사용자가 Host인지 구분해 시작 버튼 또는 준비 버튼이 될지 구분

--- Android/app/src/main/java/com/hanjan/hanjangame/GameRoomActivity.kt
+++ Android/app/src/main/java/com/hanjan/hanjangame/GameRoomActivity.kt
@@ -19,6 +19,8 @@ class GameRoomActivity : AppCompatActivity() {
         UserWithData("", "사용자 2", false, false),
         UserWithData("", "사용자 3", false, true)
     )
+    private var host = false
+    private var ready = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +39,36 @@ class GameRoomActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = UserListAdapter(userList)
         binding.recyclerView.suppressLayout(true)
+        host = true // 테스트용으로 host를 임시 지정
+        if(host){
+            binding.gameStartBtn.text = "시작"
+        } else {
+            binding.gameStartBtn.text = "준비"
+        }
+        binding.gameStartBtn.setOnClickListener {
+            //host인 경우
+            if(host){
+                //모든 유저가 준비 상태인지 확인 필요
+                //시작하면 게임 시작한다는 메시지 서버에 보낸 후 방의 모든 사용자가 받아야 함
+                //사용자는 게임 시작한다는 메시지를 받아야 다음 액티비티로 넘어가야 함
+                //액티비티가 넘어갈 때 현재 사용자가 Host인지 유무가 Boolean으로 넘어가야 함
+                val intent = Intent(this, GameListActivity::class.java)
+                intent.putExtra("host", host)
+                startActivity(intent)
+            } else {
+                //아닐 경우 준비하는 로직 필요
+                //준비 버튼을 누를 경우 준비했다는 메시지 서버로 보내야 함
+                //준비 상태일 경우 준비 해제 버튼이 되어야 함
+                //준비 해제를 할 경우 준비 해제 메시지 보내야 함
+                if(ready){//준비 해제
+                    ready = false
+                    binding.gameStartBtn.text = "준비"
+                } else {//준비
+                    ready = true
+                    binding.gameStartBtn.text = "준비 해제"
+                }
+            }
+        }
     }
 
     override fun onBackPressed() {
