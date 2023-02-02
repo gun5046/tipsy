4d5aaf0f4ea988aba88529d555f41712dcc73bed
송관재 <kosdjs39@gmail.com>
2023-02-02 13:24:34 +0900

    Feat: [S08P12D207-230, S08P12D207-231] Host만 게임 리스트가 보이게 만들기, Host를 제외한 나머지 사용자는 게임 선택 중이라는 표시 해주기

--- Android/app/src/main/java/com/hanjan/hanjangame/GameListActivity.kt
+++ Android/app/src/main/java/com/hanjan/hanjangame/GameListActivity.kt
@@ -1,20 +1,54 @@
 package com.hanjan.hanjangame
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hanjan.hanjangame.adapter.GameListRecyclerViewAdapter
 import com.hanjan.hanjangame.databinding.ActivityGameListBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class GameListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGameListBinding
+    private var host = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameListBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.recyclerView.adapter = GameListRecyclerViewAdapter(true, this)
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        host = intent.getBooleanExtra("host", false)
+        if(host){
+            binding.gameListWaitText.visibility = View.GONE
+            binding.recyclerView.visibility = View.VISIBLE
+            binding.recyclerView.layoutManager = LinearLayoutManager(this)
+            binding.recyclerView.adapter = GameListRecyclerViewAdapter(host, this)
+        } else {
+            //텍스트에 fade-in, fade-out 애니메이션 넣어서 자연스럽게 처리
+            CoroutineScope(Dispatchers.IO).launch {
+                while (true){
+                    runOnUiThread {
+                        ObjectAnimator.ofFloat(binding.gameListWaitText, "alpha", 1f, 0f).apply {
+                            duration = 500
+                            start()
+                        }
+                    }
+                    delay(500)
+                    runOnUiThread {
+                        ObjectAnimator.ofFloat(binding.gameListWaitText, "alpha", 0f, 1f).apply {
+                            duration = 500
+                            start()
+                        }
+                    }
+                    delay(500)
+                }
+            }
+            binding.gameListWaitText.visibility = View.VISIBLE
+            binding.recyclerView.visibility = View.GONE
+        }
     }
 }
