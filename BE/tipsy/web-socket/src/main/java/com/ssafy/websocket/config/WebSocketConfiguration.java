286ab280e19f6a61a09c108bc587fa742e5f79e8
송관재 <kosdjs39@gmail.com>
2023-02-10 14:14:41 +0900

    Fix: 웹에서 소켓 연결할 때 CORS 문제 해결

--- BE/tipsy/web-socket/src/main/java/com/ssafy/websocket/config/WebSocketConfiguration.java
+++ BE/tipsy/web-socket/src/main/java/com/ssafy/websocket/config/WebSocketConfiguration.java
@@ -18,6 +18,6 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").withSockJS();
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*").withSockJS();
     } // 
 }
