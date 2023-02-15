5ac504eb0b0d8257fe275ac09bcf7d4e42e21ab4
dnflwhalswl <dnflwhalswl@naver.com>
2023-02-15 22:30:08 +0900

    Fix: fix uid null error

--- BE/tipsy/tipsy-room/src/main/java/com/ssafy/tipsyroom/controller/RoomController.java
+++ BE/tipsy/tipsy-room/src/main/java/com/ssafy/tipsyroom/controller/RoomController.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
